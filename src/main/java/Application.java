4d33492aee1665769d74f256019ba9c27590e470
Maksim Uskov <MUskov87@mail.ru>
2021-11-03 22:59:32 +0400

    initial commi

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
 public interface Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world2");
+     int x =10;
+     int a = x *2;
+     int b = 7;
+     int c = a+b;
+        System.out.println(c);
     }
 }
